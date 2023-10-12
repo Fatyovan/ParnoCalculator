b4e58b212b9b1f2d7a48f0fa14231bd32d2fdbc6
Ivan Jovanovik <Ivan.jovanovik@vpar.com>
2023-10-12 08:36:23 +0200

    Initial Commit

---
+++ ParnoCalculator/ParnoCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParnoCalculatorApp.swift
+//  ParnoCalculator
+//
+//  Created by Ivan Jovanovik on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParnoCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
